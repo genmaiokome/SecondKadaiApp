3a49ab6d55a85cb0c591ff5c130b0e0fd6ac122d
ryosuke.watanabe <genmaiokomey@yahoo.co.jp>
2020-10-02 11:37:12 +0900

    Initial Commit

---
+++ SecondKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondKadaiApp
+//
+//  Created by 渡辺涼介 on 2020/10/02.
+//  Copyright © 2020 ryosuke.watanabe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
